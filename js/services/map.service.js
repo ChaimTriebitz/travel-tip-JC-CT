19c81660ed5d23eb369597731b7fd18f420f48d6
josh collins <joshcoll94@gmail.com>
2022-08-17 23:37:25 +0300

    css html

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -36,7 +36,6 @@ function panTo(lat, lng) {
     gMap.panTo(laLatLng)
 }
 
-
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
     const API_KEY = '' //TODO: Enter your API Key
